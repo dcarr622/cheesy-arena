6837d5a15fe22c601077fa6be7386bba9a5f692f
David Carr <davidc@twitter.com>
2016-10-09 07:57:56 -0700

    Enlarge bottom overlay for audience display

--- static/js/audience_display.js
+++ static/js/audience_display.js
@@ -128,7 +128,7 @@ var handleLowerThird = function(data) {
 var transitionBlankToIntro = function(callback) {
   $("#centering").transition({queue: false, bottom: "0px"}, 500, "ease", function() {
     $(".teams").transition({queue: false, width: "65px"}, 100, "linear", function() {
-      $(".score").transition({queue: false, width: "120px"}, 500, "ease", function() {
+      $(".score").transition({queue: false, width: "410px"}, 500, "ease", function() {
         $("#eventMatchInfo").show();
         var height = -$("#eventMatchInfo").height();
         $("#eventMatchInfo").transition({queue: false, bottom: height + "px"}, 500, "ease", callback);
@@ -139,7 +139,7 @@ var transitionBlankToIntro = function(callback) {
 
 var transitionIntroToInMatch = function(callback) {
   $("#logo").transition({queue: false, top: "25px"}, 500, "ease");
-  $(".score").transition({queue: false, width: "250px"}, 500, "ease", function() {
+  $(".score").transition({queue: false, width: "410px"}, 500, "ease", function() {
     $(".score-number").transition({queue: false, opacity: 1}, 750, "ease");
     $(".score-fields").transition({queue: false, opacity: 1}, 750, "ease");
     $("#matchTime").transition({queue: false, opacity: 1}, 750, "ease", callback);
@@ -160,7 +160,7 @@ var transitionBlankToInMatch = function(callback) {
   $("#centering").transition({queue: false, bottom: "0px"}, 500, "ease", function() {
     $(".teams").transition({queue: false, width: "65px"}, 100, "linear", function() {
       $("#logo").transition({queue: false, top: "25px"}, 500, "ease");
-      $(".score").transition({queue: false, width: "250px"}, 500, "ease", function() {
+      $(".score").transition({queue: false, width: "410px"}, 500, "ease", function() {
         $("#eventMatchInfo").show();
         $(".score-number").transition({queue: false, opacity: 1}, 750, "ease");
         $(".score-fields").transition({queue: false, opacity: 1}, 750, "ease");
@@ -177,7 +177,7 @@ var transitionInMatchToIntro = function(callback) {
   $(".score-fields").transition({queue: false, opacity: 0}, 300, "linear");
   $("#matchTime").transition({queue: false, opacity: 0}, 300, "linear", function() {
     $("#logo").transition({queue: false, top: "40px"}, 500, "ease");
-    $(".score").transition({queue: false, width: "120px"}, 500, "ease");
+    $(".score").transition({queue: false, width: "410px"}, 500, "ease");
     $(".teams").transition({queue: false, width: "65px"}, 500, "ease", callback);
   });
 };
